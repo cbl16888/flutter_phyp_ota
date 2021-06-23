97d3fcf3f66e49e1849eef1ef7217e3e276ec2bb
chenboling <chenboling@dkhs.com>
2021-06-23 09:07:12 +0800

    项目运行

--- lib/flutter_phyp_ota.dart
+++ lib/flutter_phyp_ota.dart
@@ -1,11 +1,9 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class FlutterPhypOta {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_phyp_ota');
+  static const MethodChannel _channel = const MethodChannel('flutter_phyp_ota');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
